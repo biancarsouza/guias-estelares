00e8a75db80ffdfdfff4635f90932c989857d378
Bianca <biancaribeiro2004@gmail.com>
2021-06-08 22:48:28 -0300

    Update 6

--- Guia estelar de JS/script.js
+++ Guia estelar de JS/script.js
@@ -3,4 +3,29 @@
 /* Comentário
 em mais de uma linha. */
 
-console.log('hola cuerno');
+console.log("Aqui vem uma string.");
+console.log('Outra string.');;
+console.log(`A crase possibilita realizar expressões dentro de um string, como essa: ${1 + 1}.`);
+
+// Criando um objeto:
+
+console.log({
+
+    nome: "Bianca",
+    idade: 17
+
+})
+
+// Criando uma variável.
+
+var clima = "Quente";
+let clima2 = "Frio";
+const clima3 = "Ameno"; // A const, diferente do let e do var, não pode mudar no meio do programa.
+
+console.log(clima);
+console.log(clima2);
+console.log(clima3);
+
+let teste = true;
+
+console.log(typeof teste);
